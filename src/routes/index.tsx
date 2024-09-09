8c6ad50c24a9d9ec9f5c88f8abcf1223ca8d006c
h.bussolari <h.bussolari@quattror.com.br>
2024-09-09 10:20:47 -0300

    includes product details route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "../components/Pages/Home/Home"
+import ProductDetails from "../components/Pages/ProductDetails/ProductDetails"
 
 
 const AppRoutes = () => {
@@ -7,6 +8,7 @@ const AppRoutes = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Home />}/>
+                <Route path="/details/:id" element={<ProductDetails />} />
             </Routes>
         </Router>
     )
