5c25da07a82c95241f3be20ada2a00d9dc508c43
h.bussolari <h.bussolari@quattror.com.br>
2024-09-09 11:15:40 -0300

    Adds vite module federation config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@originjs/vite-plugin-federation';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: 'tabela-produtos',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Home': './src/components/Pages/Home/Home.tsx', // Exposing Home
+        './ProductDetails': './src/components/Pages/ProductDetails/ProductDetails.tsx', // Exposing Product Details
+      },
+      remotes: {
+        // otherApp: 'otherApp@http://localhost:3001/remoteEntry.js', // URL of remote app's remoteEntry.js file
+        // anotherRemote: 'anotherRemote@http://localhost:3002/remoteEntry.js', // Another remote app URL
+      },
+      shared: ['react', 'react-dom'], // Dependencies that should be shared between federated modules
+    }),
+  ],
+  build: {
+    target: 'esnext',
+  },
+});
